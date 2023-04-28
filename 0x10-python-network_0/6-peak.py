f42333c4486ba972193fe29b74b2b4fb1d9ece2e
isgameliel <simvicpro@gmail.com>
2023-04-28 05:26:55 +0100

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
         return None
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
 
-    mid_idx = int(len(list_of_integers) / 2)
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
 
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-        else:
-            return list_of_integers[mid_idx - 1]
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
 
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
