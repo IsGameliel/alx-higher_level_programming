0a9605b0c5160e5064736b025b7e4e278252a2cc
isgameliel <simvicpro@gmail.com>
2023-05-31 22:03:48 +0100

    jquery

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    $('INPUT#btn_search').click(function () {
+      $.ajax({
+        type: 'GET',
+        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + $('INPUT#city_search').val() + '%22)&format=json',
+        success: function (data) {
+          let input = $('INPUT#city_search').val();
+          if (input) {
+            $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
+          }
+        }
+      });
+    });
+
+    $('INPUT#city_search').keypress(function (e) {
+      if (e.which === 13) {
+        $('INPUT#btn_search').click();
+      }
+    });
+  });
+});
